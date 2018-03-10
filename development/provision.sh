73db4853d372862326cf74c9127adfbea529b287
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-03-10 15:10:00 -0500

    development: Creates rabbitmq admin user.

--- development/provision.sh
+++ development/provision.sh
@@ -22,3 +22,11 @@ cd /vagrant/
 # Then, we run nixos-rebuild switch *using the global configuration.nix*.
 # which includes the vagrant bits in which we are overriding a small bit.
 nix-shell --run "NIX_PATH='$NIX_PATH:nix-config=/etc/nixos/configuration.nix' sudo nixos-rebuild switch --show-trace"
+
+# Creates admin user...
+(
+RABBITMQ_USE_LONGNAME=true HOME=/var/lib/rabbitmq rabbitmqctl add_user admin admin
+RABBITMQ_USE_LONGNAME=true HOME=/var/lib/rabbitmq rabbitmqctl set_user_tags admin administrator
+RABBITMQ_USE_LONGNAME=true HOME=/var/lib/rabbitmq rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
+) || :
+# Without failing (e.g. it already exists).
